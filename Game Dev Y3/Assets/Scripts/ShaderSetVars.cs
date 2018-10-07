0551cf208a6d6443bb06e917eb5564021b75bb66
JonWaaler <jonwaaler@outlook.com>
2018-10-07 15:11:49 -0400

    Prototpye Init

---
+++ Game Dev Y3/Assets/Scripts/ShaderSetVars.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderSetVars : MonoBehaviour {
+
+    public Material mat;
+    public AnimationCurve t;
+    private float t_Material = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        t_Material += Time.deltaTime;
+
+        mat.SetFloat("_offset", t.Evaluate(t_Material));
+
+        if (t_Material >= 1)
+        {
+            mat.SetFloat("_offset", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            t_Material = 0.88f;
+        }
+
+    }
+}
