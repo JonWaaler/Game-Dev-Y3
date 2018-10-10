3bf4f0cd1d93c2a0a0e34d76bf74c6bb13326bf3
Mitchell Fukuzawa <mitchellfukuzawa@hotmail.com>
2018-10-10 17:53:37 -0400

    +Added Jumping

--- Game Dev Y3/Assets/Scripts/Player.cs
+++ Game Dev Y3/Assets/Scripts/Player.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	private float xVel;
-	private float yVel;
+	private float zvel;
 	private float xFire;
 	private float yFire;
 	private Vector3 inputVector;
 
 	public float speed;
-	public string H_LS_PNum, V_LS_PNum, H_RS_PNum, V_RS_PNum;
+	public string H_LS_PNum, V_LS_PNum, H_RS_PNum, V_RS_PNum, AButton_PNum;
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2f;
+	[Range(1, 10)]
+	public float jumpVelocity;
 
 
 	// Use this for initialization
@@ -22,19 +26,19 @@ public class Player : MonoBehaviour {
 	void Update ()
 	{
 		xVel = Input.GetAxis(H_LS_PNum);
-		yVel = Input.GetAxis(V_LS_PNum);
+		zvel = Input.GetAxis(V_LS_PNum);
 
-		inputVector = new Vector3(xVel, 0, yVel);
+		inputVector = new Vector3(xVel, 0, zvel);
 
 		//print("X_Vel: "+ xVel);
-		//print("z_Vel: "+ yVel);
+		//print("z_Vel: "+ zvel);
 
-		if (xVel != 0 || yVel != 0)
+		if (xVel != 0 || zvel != 0)
 		{
 			GetComponent<Rigidbody>().AddForce(inputVector.normalized * speed);
 		}
 
-		GetComponent<Rigidbody>().velocity = new Vector3 (speed * xVel, -9.8f, speed * yVel);
+		GetComponent<Rigidbody>().velocity = new Vector3 (speed * xVel, GetComponent<Rigidbody>().velocity.y, speed * zvel);
 
 		Vector3 playerDirection = Vector3.right * Input.GetAxisRaw(H_RS_PNum) + Vector3.forward * -Input.GetAxisRaw(V_RS_PNum);
 		if(playerDirection.sqrMagnitude > 0.0f)
@@ -45,6 +49,22 @@ public class Player : MonoBehaviour {
 
         }
 
+		if (Input.GetButtonDown(AButton_PNum) && GetComponent<Rigidbody>().velocity.y == 0)
+		{
+			GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+		}
+		if (GetComponent<Rigidbody>().velocity.y < 0)
+		{
+			GetComponent<Rigidbody>().velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
+		else if (GetComponent<Rigidbody>().velocity.y > 0 && !Input.GetButton(AButton_PNum))
+		{
+			GetComponent<Rigidbody>().velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
 
+	}	
+	void FixedUpdate()
+	{
+		
 	}
 }
