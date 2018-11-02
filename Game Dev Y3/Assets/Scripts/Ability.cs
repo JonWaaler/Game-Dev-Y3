52a438ed646ec5bf12dd07dd3d7c93ee28008a0e
Nathan <juthun@hotmail.com>
2018-11-02 17:04:24 -0400

    edited Ability.cs // not in a pattern yet

--- Game Dev Y3/Assets/Scripts/Ability.cs
+++ Game Dev Y3/Assets/Scripts/Ability.cs
@@ -7,7 +7,7 @@ public class Ability : MonoBehaviour {
     public string BButton_PNum;
     public float abilCool = 1;
     public float distance = 10.0f;
-    public float rollTime = 0.5f;
+    public float lerpSpeed = 20.0f;
 
     //    private WaitForSeconds lerpDuration = new WaitForSeconds(2);
     private float rollDistance = 10.0f;
@@ -29,14 +29,13 @@ public class Ability : MonoBehaviour {
             {
                 if (hit.collider.tag.Equals("Wall"))
                 {
-                    if (Mathf.Abs( hit.distance) < 1.75f)
+                    if (Mathf.Abs( hit.distance) < 1.25f)
                     {
                         rollDistance = 0.0f;
                     }
                     else
                     {
                         rollDistance = hit.distance;
-                        print("Raycast hit: " + hit.transform.name);
                     }
                 }
                 else
@@ -48,32 +47,38 @@ public class Ability : MonoBehaviour {
                 }
                     
                 Vector3 hitPoint =  transform.position + (transform.forward * (rollDistance * .8f));
-                StartCoroutine(Roll(transform.position, hitPoint, rollTime));
+                StartCoroutine(Roll(transform.position, hitPoint, lerpSpeed));
             }
             else
             {
                 rollDistance = distance;
-                StartCoroutine(Roll(transform.position, transform.position + (transform.forward * rollDistance), rollTime));
+                StartCoroutine(Roll(transform.position, transform.position + (transform.forward * rollDistance), lerpSpeed));
             }
         }
 
     }
 
-    private IEnumerator Roll(Vector3 origin, Vector3 target, float lerpTime)
+    private IEnumerator Roll(Vector3 origin, Vector3 target, float velocity)
     {
-        float current = 0.0f;
         string gameObjTag = gameObject.tag;
-//        Color originColor =  <Material>().color;
-  //      gameObject.GetComponentInChildren<Material>().color = Color.green;
-        while (current <= lerpTime)
+        Color originColor = transform.GetChild(0).GetComponent<Renderer>().material.color;
+        gameObject.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+
+        float current = 0.0f;//Elapsed time
+        float rollLengh = (target - origin).magnitude; //Distance
+        float totalTime = rollLengh / velocity; // Total time to finish distance at said velocity with: T = D/V
+
+        while (current <= totalTime)
         {
             gameObject.tag = "Wall";
-            current = current + Time.deltaTime;
-            float percent = Mathf.Clamp01(current / lerpTime);
-            GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(origin, target, percent));
+
+            current += Time.deltaTime; // Elapsed time
+            float tValue = Mathf.Clamp01(current / totalTime); // figure out how much of % time has passed of elaped time relative to total time 
+            GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(origin, target, tValue));
+
             yield return null;
         }
-//        gameObject.GetComponentInChildren<Material>().color = originColor;
+        gameObject.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", originColor);
         gameObject.tag = gameObjTag;
     }
 
