d406ee209aca86a0955810b8dfa897544230f111
JonWaaler <jonwaaler@outlook.com>
2018-11-03 17:18:14 -0400

    Map design

--- Game Dev Y3/Assets/Scripts/TerritoryBehavior.cs
+++ Game Dev Y3/Assets/Scripts/TerritoryBehavior.cs
@@ -7,13 +7,17 @@ public class TerritoryBehavior : MonoBehaviour {
 
     public enum TerritoryType { Damage, Regen };
     public TerritoryType terrioryType;
-    private Slider player1_Health;
+    public Slider player1_Health;
     public Slider player2_Health;
+    public Slider player3_Health;
+    public Slider player4_Health;
 
     void Start()
     {
         player1_Health = GameObject.Find("Player 1 - Health").GetComponent<Slider>();
         player2_Health = GameObject.Find("Player 2 - Health").GetComponent<Slider>();
+        player3_Health = GameObject.Find("Player 3 - Health").GetComponent<Slider>();
+        player4_Health = GameObject.Find("Player 4 - Health").GetComponent<Slider>();
     }
 
     // When player touching collider, give damage
@@ -43,6 +47,14 @@ public class TerritoryBehavior : MonoBehaviour {
         if (collision.gameObject.name == "Player Parent 2")
         {
             player2_Health.value += Time.deltaTime;
+        }
+        if (collision.gameObject.name == "Player Parent 3")
+        {
+            player3_Health.value += Time.deltaTime;
+        }
+        if (collision.gameObject.name == "Player Parent 4")
+        {
+            player4_Health.value += Time.deltaTime;
         }
     }
 
